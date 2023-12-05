d97f0a2eac5346f3a66797dc3cea4b606f1e8c8b
Jaroslav <garasym@gmail.com>
2023-12-05 20:37:42 +0200

    GreenCityLoginInvalid class example

---
+++ src/test/java/com/softserve/edu02/GreenCityLoginValid.java
@@ -0,0 +1,176 @@
+package com.softserve.edu02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class GreenCityLoginValid {
+
+
+    private static final String BASE_URL = "https://www.greencity.social/#/ubs";
+    private static final Long IMPLICITLY_WAIT_SECONDS = 10L;
+    private static final Long ONE_SECOND_DELAY = 1000L;
+    private static WebDriver driver;
+
+    // Overload
+    protected void presentationSleep() {
+        presentationSleep(1);
+    }
+
+    // Overload
+    protected void presentationSleep(int seconds) {
+        try {
+            Thread.sleep(seconds * ONE_SECOND_DELAY); // For Presentation ONLY
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        //
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--remote-allow-origins=*");
+        //driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+        //
+        //driver.manage().timeouts().implicitlyWait(IMPLICITLY_WAIT_SECONDS, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(800, 550));
+        System.out.println("@BeforeAll executed");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit(); // close()
+        }
+        System.out.println("@AfterAll executed");
+    }
+
+    @BeforeEach
+    public void setupThis() {
+        driver.get(BASE_URL);
+        System.out.println("\t@BeforeEach executed");
+    }
+
+    @AfterEach
+    public void tearThis() {
+        //
+        // Logout. get Username
+        //driver.findElement(By.cssSelector("li.body-2")).click();
+        //driver.findElement(By.cssSelector("li[aria-label='sign-out'] > a")).click();
+        //
+        driver.manage().deleteAllCookies();
+        // TODO delete token
+        // Refresh page
+        presentationSleep(4); // For Presentation ONLY
+        //
+        System.out.println("\t@AfterEach executed");
+    }
+
+    @Test
+    public void checkLogin() {
+        //
+        // Steps TC
+        // Click Login Button
+        //driver.findElement(By.cssSelector("a.ubs-header-sign-in")).click();
+        //driver.findElement(By.cssSelector(".ubs-header-sing-in-img")).click();
+        driver.findElement(By.cssSelector("app-header:nth-child(1) .ubs-header-sing-in-img")).click();
+        //
+        // Burger menu click
+        //driver.findElement(By.cssSelector("div.menu-icon-wrapper")).click();
+        //driver.findElement(By.cssSelector("a.tertiary-global-button")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Fill email input
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("tyv09754@zslsz.com");
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Fill password input
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Qwerty_1");
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Press Button Login
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        // get Username
+        String actualUserName = driver.findElement(By.cssSelector("li.ubs-user-name")).getText();
+        String expectedUserName = "QwertyY";
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Check
+        Assertions.assertEquals(expectedUserName, actualUserName);
+        presentationSleep(); // For Presentation ONLY
+        //
+        System.out.println("\t\tTest testUi() executed");
+    }
+
+    @Test
+    public void checkMyHabit() {
+        //
+        // Steps TC
+        // Click Login Button
+        //driver.findElement(By.cssSelector("a.ubs-header-sign-in")).click();
+        //driver.findElement(By.cssSelector(".ubs-header-sing-in-img")).click();
+        driver.findElement(By.cssSelector("app-header:nth-child(1) .ubs-header-sing-in-img")).click();
+        //
+        // Burger menu click
+        //driver.findElement(By.cssSelector("div.menu-icon-wrapper")).click();
+        //driver.findElement(By.cssSelector("a.tertiary-global-button")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Fill email input
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("tyv09754@zslsz.com");
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Fill password input
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Qwerty_1");
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Press Button Login
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        // get Username
+        String actualUserName = driver.findElement(By.cssSelector("li.ubs-user-name")).getText();
+        String expectedUserName = "QwertyY";
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Green City menu Click
+        //driver.findElement(By.xpath("//ul[@role='tablist']//a[contains(text(),'Green City')]")).click();
+        driver.findElement(By.cssSelector("app-header:nth-child(1) ul[role='tablist'] a[href*='greenCity']")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Start forming a habit button click
+        driver.findElement(By.cssSelector("div#main-content button.primary-global-button.btn")).click();
+        presentationSleep(); // For Presentation ONLY
+        //
+        String actualLabel = driver.findElement(By.cssSelector("div[aria-selected='true'] div.mat-tab-label-content")).getText();
+        String expectedLabel = "My habits";
+        presentationSleep(); // For Presentation ONLY
+        //
+        // Check
+        Assertions.assertTrue(actualLabel.contains(expectedLabel));
+        presentationSleep(); // For Presentation ONLY
+        //
+        System.out.println("\t\tTest testUi() executed");
+    }
+
+}
