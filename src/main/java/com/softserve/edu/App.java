5c5c6ee4d6042e03cb3d36b8af2244f63a4e51fb
Jaroslav <garasym@gmail.com>
2023-12-15 20:08:34 +0200

    Slf4j+Log4j

---
+++ src/main/java/com/softserve/edu/App.java
@@ -0,0 +1,19 @@
+package com.softserve.edu;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        Thread.sleep(1);
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        throw new RuntimeException("Error Error Error Error Error Error Error");
+    }
+}
